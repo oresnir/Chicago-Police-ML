ee25b57171d859f93f6178c5b8ac074f0dcca2a7
evyatarkar <80752596+evyatarkar@users.noreply.github.com>
2021-06-03 00:28:26 +0300

    hi

--- pre_proc.py
+++ pre_proc.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 import scipy.stats as sp
 # from pdpbox.pdp import pdp_isolate, pdp_plot
 import sns as sns
 from sklearn.model_selection import train_test_split
+
+target = 'Primary Type'
 
 df = pd.read_csv('../Chicago-Police-ML/Dataset_crimes.csv', sep=",")
 
@@ -13,8 +16,8 @@ df = pd.read_csv('../Chicago-Police-ML/Dataset_crimes.csv', sep=",")
 #
 # plt.show()
 
-labels = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE',
-          3: 'DECEPTIVE PRACTICE', 4: 'ASSAULT'}
+labels = {'BATTERY': 0, 'THEFT': 1,  'CRIMINAL DAMAGE': 2,
+          'DECEPTIVE PRACTICE': 3, 'ASSAULT': 4}
 
 
 def split_data(data):
@@ -34,12 +37,6 @@ def clean_data(data):  # receives X m*d
     # delete null values
     data = data.dropna()
 
-    #delete outliers TODO CHECK
-    # z_score = sp.zscore(data)
-    # abs_z_score = np.abs(z_score)
-    # filter_entire = (abs_z_score < 3).all(axis=1)
-    # data = data[filter_entire]
-
     # delete irrelevant fetcher
     data = data.drop('Unnamed: 0', axis=1)
     data = data.drop('ID', axis=1)
@@ -52,11 +49,35 @@ def clean_data(data):  # receives X m*d
     data = data.drop('Description', axis=1)
     data = data.drop('IUCR', axis=1)
     data = data.drop('FBI Code', axis=1)
+
+    # changes boll to int
     data['Arrest'] = data['Arrest'].astype(int)
     data['Domestic'] = data['Domestic'].astype(int)
 
+    # Splitting the Date to Day, Month, Year, Hour, Minute, Second
+    data['date2'] = pd.to_datetime(df['Date'])
+    data['Year'] = data['date2'].dt.year
+    data['Month'] = data['date2'].dt.month
+    data['Day'] = data['date2'].dt.day
+    data['Hour'] = data['date2'].dt.hour
+    data['Minute'] = data['date2'].dt.minute
+    # data['Second'] = data['date2'].dt.second
+    data = data.drop(['Date'], axis=1)
+    data = data.drop(['date2'], axis=1)
+    data = data.drop(['Updated On'], axis=1)
+
     # data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y %H:%M:%S %p', errors='coerce')
     data = data.drop_duplicates()
+
+    # data['Primary Type'] = LabelEncoder().fit_transform(data['Primary Type'])
+    c = pd.Categorical(data['Primary Type'])
+    data['Primary Type'] = c.rename_categories(labels)
+
+    #delete outliers TODO
+    # z_score = sp.zscore(data)
+    # abs_z_score = np.abs(z_score)
+    # filter_entire = (abs_z_score < 3).all(axis=1)
+    # data = data[filter_entire]
 
     # print(data)
     return data
@@ -65,9 +86,9 @@ def clean_data(data):  # receives X m*d
 if __name__ == '__main__':
     full_data = pd.read_csv("dataset_crimes.csv")
     train, validate, test = split_data(full_data)
-    # print(train.dtypes)
+    print(train[target])
     train = clean_data(train)
-    print(train.dtypes)
+    print(train[target])
 
 
 
