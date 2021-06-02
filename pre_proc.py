06c331543c044863babc31b57edc6a6caf71a49a
oresnir <snirore@gmail.com>
2021-06-02 22:37:39 +0300

    commit

--- pre_proc.py
+++ pre_proc.py
@@ -1,43 +1,50 @@
-import gc
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import psutil
-import seaborn as sns
 # from pdpbox.pdp import pdp_isolate, pdp_plot
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import mean_absolute_error
+import sns as sns
 from sklearn.model_selection import train_test_split
-from sklearn.utils.multiclass import unique_labels
 
-df= pd.read_csv('../data_set/Chicago_Crimes_2015_to_2017.csv', sep=",")
+df = pd.read_csv('../Chicago-Police-ML/Dataset_crimes.csv', sep=",")
 
-#Drop nan values
-df= df.dropna()
+# Drop nan values
+df = df.dropna()
 
-
-#Drop column Unnamed
-df=df.drop('Unnamed: 0',axis=1)
+# Drop column Unnamed
+df = df.drop('Unnamed: 0', axis=1)
 df.head()
 
-#Convert Date column to Date Time format
-df['Date']= pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S %p' , errors='coerce')
+# Convert Date column to Date Time format
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S %p', errors='coerce')
 
-#Eliminate duplicate rows
-df=df.drop_duplicates()
+# Eliminate duplicate rows
+df = df.drop_duplicates()
 
-#Visualization of the Longitude and Latitude.
+# Visualization of the Longitude and Latitude.
 plt.scatter('Longitude', 'Latitude', c='gray', data=df, s=20)
 
 plt.show();
 
-labels = {0:'BATTERY', 1:'THEFT', 2:'CRIMINAL DAMAGE',
-          3:'DECEPTIVE RACTICE', 4:'ASSAULT'}
+labels = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE',
+          3: 'DECEPTIVE RACTICE', 4: 'ASSAULT'}
+
+
+def split_data():
+    # Split Data Train/ Validate/Test
+    target = 'Arrest'  # our labels
+    features = df.columns.drop([target, 'ID'])
+
+    X = df[features]
+    y = df[target]
+
+    # split to train and test data
+    X_trainval, X_test, y_trainval, y_test = train_test_split(
+        X, y, train_size=0.80, test_size=0.20, random_state=42)
+
+    # create validation data
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_trainval, y_trainval, test_size=0.2, random_state=42)
+
 
 def clean_data(data):  # receives X m*d
     data = data.dropna()
@@ -55,26 +62,6 @@ def clean_data(data):  # receives X m*d
 
     print(data)
 
-    df = pd.read_csv('../data_set/Chicago_Crimes_2015_to_2017.csv', sep=",")
-
-    # Drop nan values
-    df = df.dropna()
-
-    # Drop column Unnamed
-    df = df.drop('Unnamed: 0', axis=1)
-    df.head()
-
-    # Convert Date column to Date Time format
-    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S %p', errors='coerce')
-
-    # Eliminate duplicate rows
-    df = df.drop_duplicates()
-
-    # Visualization of the Longitude and Latitude.
-    plt.scatter('Longitude', 'Latitude', c='gray', data=df, s=20)
-
-    plt.show();
-
 
 if __name__ == '__main__':
     full_data = pd.read_csv("dataset_crimes.csv")
@@ -85,9 +72,11 @@ if __name__ == '__main__':
     print(train)
     clean_data(train)
 
-    # sns.set(style='ticks', context='talk')
-    # sns.swarmplot(x='Domestic', y='Primary Type', data=test)
-    # sns.despine()
-    # print(train.describe())
-    # plt.matshow(test)
-    # plt.show()
+
+# TODO: ORE TO RON& EVYA:  WHAT IS THIS?
+sns.set(style='ticks', context='talk')
+sns.swarmplot(x='Domestic', y='Primary Type', data=test)
+sns.despine()
+print(train.describe())
+plt.matshow(test)
+plt.show()
