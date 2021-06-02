1b5753b8f2e11ac6c6edb154f48ee6faa6dd3c69
oresnir <snirore@gmail.com>
2021-06-02 22:24:16 +0300

    commit

--- pre_proc.py
+++ pre_proc.py
@@ -1,18 +1,40 @@
-import pandas as pd
+import gc
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import seaborn as sns
-import matplotlib.pyplot as plt
-from pdpbox.pdp import pdp_isolate, pdp_plot
-import gc
 import psutil
-import os
+import seaborn as sns
+# from pdpbox.pdp import pdp_isolate, pdp_plot
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.utils.multiclass import unique_labels
+
+df= pd.read_csv('../data_set/Chicago_Crimes_2015_to_2017.csv', sep=",")
+
+#Drop nan values
+df= df.dropna()
+
+
+#Drop column Unnamed
+df=df.drop('Unnamed: 0',axis=1)
+df.head()
+
+#Convert Date column to Date Time format
+df['Date']= pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S %p' , errors='coerce')
+
+#Eliminate duplicate rows
+df=df.drop_duplicates()
+
+#Visualization of the Longitude and Latitude.
+plt.scatter('Longitude', 'Latitude', c='gray', data=df, s=20)
+
+plt.show();
 
 labels = {0:'BATTERY', 1:'THEFT', 2:'CRIMINAL DAMAGE',
           3:'DECEPTIVE RACTICE', 4:'ASSAULT'}
@@ -32,6 +54,26 @@ def clean_data(data):  # receives X m*d
     # data = data.drop('FBI Code', axis=1)
 
     print(data)
+
+    df = pd.read_csv('../data_set/Chicago_Crimes_2015_to_2017.csv', sep=",")
+
+    # Drop nan values
+    df = df.dropna()
+
+    # Drop column Unnamed
+    df = df.drop('Unnamed: 0', axis=1)
+    df.head()
+
+    # Convert Date column to Date Time format
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S %p', errors='coerce')
+
+    # Eliminate duplicate rows
+    df = df.drop_duplicates()
+
+    # Visualization of the Longitude and Latitude.
+    plt.scatter('Longitude', 'Latitude', c='gray', data=df, s=20)
+
+    plt.show();
 
 
 if __name__ == '__main__':
