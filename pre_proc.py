4f19b68be8a1b4c02fa722c1113b3471a35057db
evyatarkar <80752596+evyatarkar@users.noreply.github.com>
2021-06-03 11:21:14 +0300

    hi

--- pre_proc.py
+++ pre_proc.py
@@ -7,31 +7,29 @@ import scipy.stats as sp
 # import sns as sns
 from sklearn.model_selection import train_test_split
 
-target = 'Primary Type'
 
-df = pd.read_csv('../Chicago-Police-ML/Dataset_crimes.csv', sep=",")
-
+# df = pd.read_csv('../Chicago-Police-ML/Dataset_crimes.csv', sep=",")
 # Visualization of the Longitude and Latitude.
 # plt.scatter('Longitude', 'Latitude', c='gray', data=df, s=20)
-#
 # plt.show()
 
+TARGET = 'Primary Type'
 labels = {'BATTERY': 0, 'THEFT': 1,  'CRIMINAL DAMAGE': 2,
           'DECEPTIVE PRACTICE': 3, 'ASSAULT': 4}
 
 
-def split_data(data):
-    # split to train and test data
-    train, validate, test = np.split(data.sample(frac=1), [int(.7 * len(df)), int(.5 * len(df))])
-    train.to_csv("train.csv")
-    validate.to_csv("validate.csv")
-    test.to_csv("test.csv")
-    return train, validate, test
+# def split_data(data):
+#     # split to train and test data
+#     train, validate, test = np.split(data.sample(frac=1), [int(.7 * len(df)), int(.5 * len(df))])
+#     train.to_csv("train.csv")
+#     validate.to_csv("validate.csv")
+#     test.to_csv("test.csv")
+#     return train, validate, test
 
 
 def split_x_y(data):
-    y = data['Primary Type']
-    X = data.drop('Primary Type', axis=1)
+    y = data[TARGET]
+    X = data.drop(TARGET, axis=1)
 
     return X, y
 
@@ -42,12 +40,14 @@ def clean_data(data):  # receives X m*d
 
     # delete irrelevant fetcher
     data = data.drop('Unnamed: 0', axis=1)
+    data = data.drop('Unnamed: 0.1', axis=1)
     data = data.drop('ID', axis=1)
     data = data.drop('Year', axis=1)
     data = data.drop('Location', axis=1)
     data = data.drop('Case Number', axis=1)
     data = data.drop('Location Description', axis=1)
     data = data.drop('Block', axis=1)
+    ata = data.drop('Updated On', axis=1)
 
     data = data.drop('Description', axis=1)
     data = data.drop('IUCR', axis=1)
@@ -58,7 +58,7 @@ def clean_data(data):  # receives X m*d
     data['Domestic'] = data['Domestic'].astype(int)
 
     # Splitting the Date to Day, Month, Year, Hour, Minute, Second
-    data['date2'] = pd.to_datetime(df['Date'])
+    data['date2'] = pd.to_datetime(data['Date'])
     data['Year'] = data['date2'].dt.year
     data['Month'] = data['date2'].dt.month
     data['Day'] = data['date2'].dt.day
@@ -69,12 +69,15 @@ def clean_data(data):  # receives X m*d
     data = data.drop(['date2'], axis=1)
     data = data.drop(['Updated On'], axis=1)
 
-    # data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y %H:%M:%S %p', errors='coerce')
     data = data.drop_duplicates()
+    # changes string labels to ints
+    c = pd.Categorical(data[TARGET])
+    data[TARGET] = c.rename_categories(labels)
 
-    # data['Primary Type'] = LabelEncoder().fit_transform(data['Primary Type'])
-    c = pd.Categorical(data['Primary Type'])
-    data['Primary Type'] = c.rename_categories(labels)
+    # normalize
+    lst = ['Beat', 'District', 'Ward', 'Community Area']
+    for i in lst:
+        data[i] = data[i] / data[i].abs().max()
 
     #delete outliers TODO
     # z_score = sp.zscore(data)
@@ -82,16 +85,16 @@ def clean_data(data):  # receives X m*d
     # filter_entire = (abs_z_score < 3).all(axis=1)
     # data = data[filter_entire]
 
-    # print(data)
     return data
 
 
 if __name__ == '__main__':
-    full_data = pd.read_csv("dataset_crimes.csv")
-    train, validate, test = split_data(full_data)
-    print(train[target])
-    train = clean_data(train)
-    print(train[target])
+    data = pd.read_csv("train.csv")
+    print(data)
+    # train, validate, test = split_data(full_data)
+    print(data['Beat'])
+    data = clean_data(data)
+    print(data['Beat'])
 
 
 
