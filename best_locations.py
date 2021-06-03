3cfb61394726456db7faaaedefc15ae1774a3fec
evyatarkar <80752596+evyatarkar@users.noreply.github.com>
2021-06-03 23:08:06 +0300

    hi

--- best_locations.py
+++ best_locations.py
@@ -1,5 +1,8 @@
 import matplotlib
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import SpectralClustering
+
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import datetime as dt
@@ -8,8 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+week_days = {"Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3,
+             "Friday": 4, "Saturday": 5, "Sunday": 6}
 labels = {'BATTERY': 0, 'THEFT': 1, 'CRIMINAL DAMAGE': 2,
           'DECEPTIVE PRACTICE': 3, 'ASSAULT': 4}
+
+cluster_dict = {}
 
 
 def split_x_y(data):
@@ -46,6 +53,11 @@ def clean_data(data):  # receives X m*d
     data['date2'] = pd.to_datetime(data['Date'])
     data['Hour'] = data['date2'].dt.hour
     data['Minute'] = data['date2'].dt.minute
+
+    data['day_of_week'] = data['date2'].dt.day_name()
+    c = pd.Categorical(data['day_of_week'])
+    data['day_of_week'] = c.rename_categories(week_days)
+
     data = data.drop('date2', axis=1)
 
     data['Date'] = pd.to_datetime(data['Date'])
@@ -76,48 +88,60 @@ def get_dist(x, y):
     return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
 
 
+class Cluster:
+
+    def __init__(self, df, type):
+        self.h = None
+        self.df = df
+        self.centers = None
+        self.type = type
+
+    def create_h(self):
+        k_means = KMeans(
+            init="random",
+            n_clusters=30,
+            n_init=10,
+            max_iter=300,
+            random_state=42)
+
+        k_means.fit(self.df)
+        self.h = k_means
+        self.centers = k_means.cluster_centers_
+
+    def plot_centers(self):
+        plt.figure(figsize=(20, 13))
+        ax = plt.axes(projection="3d")
+        ax.scatter(self.centers[:, 0], self.centers[:, 1], c=self.centers[:, 2],
+                   cmap='rainbow')
+        plt.title(f"location vs {self.type}")
+        plt.show()
+
+
 if __name__ == '__main__':
     data = pd.read_csv("train.csv")
     X, y = split_x_y(clean_data(data))
     X = X.reindex(range(X.shape[0]))
     y = y.reindex(range(X.shape[0]))
 
-    points = get_valid_points_per_date(dt.datetime(2021, 1, 7, 11, 30, 0), X)
-    print(points)
-    # Creating figure
-
-
-
-    print(X)
-    print()
-    print(y)
+    # points = get_valid_points_per_date(dt.datetime(2021, 1, 7, 11, 30, 0), X)
 
     X = X.drop('Date', axis=1)
     X = X.dropna()
     print(X.dtypes)
-    new_x = X[['X Coordinate', 'Y Coordinate', 'Hour']]
-    kmeans = KMeans(
-        init="random",
-        n_clusters=30,
-        n_init=10,
-        max_iter=300,
-        random_state=42)
-    kmeans.fit(new_x)
-    print(kmeans.labels_)
-    for i in range(len(kmeans.labels_)):
-        if kmeans.labels_[i] == 12:
-            print(new_x['Hour'][i])
-    # print(kmeans.get_params())
-    # print(kmeans.score(new_x, y))
-    fig = plt.figure(figsize=(20, 13))
-    ax = plt.axes(projection="3d")
 
-    # Creating plot
-    ax.scatter(new_x['X Coordinate'], new_x['Y Coordinate'], c=new_x['Hour'], cmap='rainbow')
-    plt.gray()
-    # plt.title("simple 3D scatter plot")
+    train_hour = X[['X Coordinate', 'Y Coordinate', 'Hour']]
+    hour_cluster = Cluster(train_hour, 'hour')
+    hour_cluster.create_h()
+    hour_cluster.plot_centers()
 
-    # show plot
-    # plt.figure()
-    # plt.scatter3d([get_dist(X['X Coordinate'][i], X['Y Coordinate'][i]) for i in range(X.shape[0])], range(X.shape[0]))
-    plt.show()
+    train_day_of_week = X[['X Coordinate', 'Y Coordinate', 'day_of_week']]
+    for key, value in week_days.items():
+        #
+        # rslt_df = dataframe[dataframe['Percentage'] > 80]
+
+        train_day_of_week = train_day_of_week[train_day_of_week['day_of_week'] == value]
+        print(train_day_of_week)
+        week_cluster = Cluster(train_day_of_week, key)
+        cluster_dict[key] = week_cluster
+        week_cluster.create_h()
+        week_cluster.plot_centers()
